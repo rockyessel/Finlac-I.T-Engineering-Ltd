b7a0cb3ef6f5a573ede776c0a747d2c18507c640
Rocky Essel <rockyessel76@gmail.com>
2023-01-13 23:20:06 +0000

    added all pages

---
+++ pages/company/privacy-policy.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1> PRIVACY POLICY</h1>
+      At [Company Name], we are committed to protecting the privacy of our
+      users. This Privacy Policy explains how we collect, use, and share
+      personal information from our users, and what rights they have in regards
+      to their personal information.
+      <div>
+        <p>Information We Collect</p>
+
+        <p>
+          We may collect personal information from our users when they create an
+          account, use our services, or interact with us in other ways. The
+          types of information we may collect include:
+        </p>
+        <ul>
+          <li>
+            Contact information, such as name, email address, and phone number
+          </li>
+          <li>Profile information, such as username and profile picture</li>
+          <li>
+            Payment information, such as credit card details and billing
+            information
+          </li>
+          <li>
+            Other information that may be relevant to our services, such as
+            preferences and usage data
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <p>How We Use the Information</p>
+
+        <p>
+          We may use the personal information we collect from our users for the
+          following purposes:
+        </p>
+        <ul>
+          <li>To provide and improve our services</li>
+          <li>To process payments and manage billing</li>
+          <li>
+            To communicate with our users, including responding to customer
+            service requests
+          </li>
+          <li>
+            To personalize our services and provide relevant content and
+            recommendations
+          </li>
+          <li>To comply with legal and regulatory requirements</li>
+        </ul>
+      </div>
+
+      <div>
+        <p>Sharing of Information</p>
+
+        <p>
+          We may share personal information with third parties in the following
+          circumstances:
+        </p>
+        <ul>
+          <li>
+            With service providers and partners who assist us in providing our
+            services
+          </li>
+          <li>
+            When required by law, such as in response to a subpoena or court
+            order
+          </li>
+          <li>
+            When necessary to protect our rights, property, or safety, or that
+            of our users or others
+          </li>
+          <li>With other third parties with your prior consent</li>
+        </ul>
+      </div>
+      
+      <div>
+        <p>Security</p>
+
+        <p>
+          We take reasonable measures to protect personal information from
+          unauthorized access, use, or disclosure. However, no security measures
+          can guarantee 100% protection. Therefore, we cannot guarantee the
+          security of the information you transmit to us.
+        </p>
+
+        <div></div>
+        <p>Users Rights</p>
+
+        <p>Users have the right to:</p>
+        <ul>
+          <li>
+            access, correct, update or request deletion of their personal
+            information
+          </li>
+          <li>object to the processing of their personal information</li>
+          <li>request that we limit the use of their personal information</li>
+          <li>request to receive</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
--- pages/company/terms-and-conditions.tsx
+++ pages/company/terms-and-conditions.tsx
@@ -1,7 +1,65 @@
 import React from 'react';
 
 const TermsAndConditions = () => {
-  return <div>TermsAndConditions</div>;
+  return (
+    <div>
+      <span> Introduction</span>
+      <p>
+        The following terms and conditions govern your use of the FEE CCTV
+        website {`(the "Website")`}. By accessing and using the Website, you
+        agree to be bound by these terms and conditions. If you do not agree to
+        these terms and conditions, you should not use the Website.
+      </p>
+      <span>Use of the Website</span>
+      <p>
+        The Website is intended for your personal, non-commercial use. You may
+        not use the Website for any illegal or unauthorized purpose. You may not
+        use the Website in any manner that could damage, disable, overburden, or
+        impair the Website or interfere with any other{` party'`}s use and
+        enjoyment of the Website.
+      </p>
+      <span>Intellectual Property</span>
+      <p>
+        The Website and all of its contents, including but not limited to text,
+        images, and code (collectively, the {`"Content"`}), are protected by
+        copyright, trademark, and other intellectual property laws. You may not
+        use the Content for any commercial purpose without the express written
+        consent of FEE.
+      </p>
+      <span>Disclaimer</span>
+      <p>
+        The Website and its Content are provided on an{' '}
+        {`"as is" and "as
+        available"`}{' '}
+        basis. FEE makes no representations or warranties of any kind, express
+        or implied, as to the operation of the Website or the Content. FEE will
+        not be liable for any damages of any kind arising from the use of the
+        Website or the Content, including but not limited to direct, indirect,
+        incidental, punitive, and consequential damages.
+      </p>
+      <span>Governing Law</span>
+      <p>
+        These terms and conditions shall be governed by and construed in
+        accordance with the laws of the country in which the FEE head office is
+        located, without giving effect to any principles of conflicts of law.
+      </p>
+      <span>Changes to the Terms and Conditions</span>
+      <p>
+        FEE reserves the right to make changes to these terms and conditions at
+        any time. Your continued use of the Website following any changes
+        indicates your acceptance of the new terms and conditions.
+      </p>
+      <span>Contact Us</span>
+      <p>
+        If you have any questions or concerns about these terms and conditions,
+        please contact us at [insert contact information].
+      </p>
+      Please note that this is a sample of terms and conditions and it should be
+      reviewed and adjusted by a legal professional to make sure that it
+      complies with the laws of your country and it covers all the aspect of
+      your business.
+    </div>
+  );
 };
 
 export default TermsAndConditions;
